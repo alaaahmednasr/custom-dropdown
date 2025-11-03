80e3a1f3523e052b1836f0aaf9424806278a2dbe
Alaa Ahmed Nasr <you@example.com>
2025-11-03 11:39:21 +0200

    error padding

--- lib/custom_dropdown.dart
+++ lib/custom_dropdown.dart
@@ -624,7 +624,7 @@ class _CustomDropdownState<T> extends State<CustomDropdown<T>> {
               errorStyle: decoration?.errorStyle ?? _defaultErrorStyle,
               errorText: formFieldState.errorText,
               border: InputBorder.none,
-              contentPadding: EdgeInsets.zero,
+              contentPadding: EdgeInsets.symmetric(horizontal: 10),
             ),
             child: _OverlayBuilder(
               overlayPortalController: widget.overlayController,
